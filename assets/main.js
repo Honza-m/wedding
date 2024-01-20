7d3636ce819536c92d053ced87e33168c45b3aff
Jan Morawiec <jan.morawiec@outlook.com>
2024-01-20 22:21:16 +0000

    Landing page fixes

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,23 @@
-if (document.getElementsByClassName("countdown").length > 0) {
-  countdown();
+const elId = "wedding-countdown";
+if (document.getElementById(elId)) {
+  countdown(elId);
 }
 
-function countdown() {
-  const weddingDate = new Date("2024-07-07T13:00:00+02:00");
+function countdown(elementId) {
+  const weddingDate = new Date(
+    document.getElementById(elementId).dataset["date"],
+  );
 
   var x = setInterval(function () {
     const diffDate = weddingDate - new Date();
+
+    // Countdown done
+    if (diffDate < 0) {
+      clearInterval(x);
+      document.getElementById(elementId).innerHTML = "It's here!";
+    }
+
+    // Countdown continues
     const days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
@@ -14,20 +25,14 @@ function countdown() {
     const minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((diffDate % (1000 * 60)) / 1000);
 
-    document.getElementsByClassName("countdown-days")[0].textContent = days;
-    document.getElementsByClassName("countdown-hours")[0].textContent = String(
-      hours,
-    ).padStart(2, "0");
-    document.getElementsByClassName("countdown-minutes")[0].textContent =
+    document.getElementsByClassName("countdown-item days")[0].textContent =
+      days;
+    document.getElementsByClassName("countdown-item hours")[0].textContent =
+      String(hours).padStart(2, "0");
+    document.getElementsByClassName("countdown-item minutes")[0].textContent =
       String(minutes).padStart(2, "0");
 
-    document.getElementsByClassName("countdown-seconds")[0].textContent =
+    document.getElementsByClassName("countdown-item seconds")[0].textContent =
       String(seconds).padStart(2, "0");
-
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementsByClassName("countdown-border").innerHTML =
-        "It's here!";
-    }
   }, 1000);
 }
