7503e77a5c4312885fd15214528b0358df5fe064
Jan Morawiec <jan.morawiec@outlook.com>
2024-01-21 22:23:12 +0000

    Improve countdowns

--- assets/main.js
+++ assets/main.js
@@ -8,30 +8,30 @@ function countdown(elementId) {
     document.getElementById(elementId).dataset["date"],
   );
 
-  var x = setInterval(function () {
-    const diffDate = weddingDate - new Date();
+  const diffDate = weddingDate - new Date();
 
-    // Countdown done
-    if (diffDate < 0) {
-      clearInterval(x);
-      document.getElementById(elementId).innerHTML = "It's here!";
-    }
+  // Countdown done
+  if (diffDate < 0) {
+    clearInterval(x);
+    document.getElementById(elementId).innerHTML = "It's here!";
+  }
 
-    // Countdown continues
-    const days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    );
-    const minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diffDate % (1000 * 60)) / 1000);
+  // Countdown continues
+  const days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diffDate % (1000 * 60)) / 1000);
 
-    document.getElementsByClassName("countdown-item days")[0].textContent =
-      String(days).padStart(3, "0");
-    document.getElementsByClassName("countdown-item hours")[0].textContent =
-      String(hours).padStart(2, "0");
-    document.getElementsByClassName("countdown-item minutes")[0].textContent =
-      String(minutes).padStart(2, "0");
-    document.getElementsByClassName("countdown-item seconds")[0].textContent =
-      String(seconds).padStart(2, "0");
-  }, 1000);
+  document.getElementsByClassName("countdown-item days")[0].textContent =
+    String(days).padStart(3, "0");
+  document.getElementsByClassName("countdown-item hours")[0].textContent =
+    String(hours).padStart(2, "0");
+  document.getElementsByClassName("countdown-item minutes")[0].textContent =
+    String(minutes).padStart(2, "0");
+  document.getElementsByClassName("countdown-item seconds")[0].textContent =
+    String(seconds).padStart(2, "0");
+
+  setTimeout(countdown, 1000, elementId);
 }
